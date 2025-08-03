b8b9272eaa3ae8fbf56de46265cdc18902c50d90
Mahdi Hassan <mahdioffi333@gmail.com>
2025-08-04 02:59:04 +0600

    update

--- variables-&-Data-Types/averageMarks.cpp
+++ variables-&-Data-Types/averageMarks.cpp
@@ -13,11 +13,11 @@ int main(){
   cout<<"Enter your Bangla mark: ";
   cin>>bangla;
 
-  cout<<"Enter your Math mark: ";
+//   cout<<"Enter your Math mark: ";
   cin>>math;
 
   int average = (english + bangla + math) / 3;
-  cout<<"Your average mark is: "<<average<<endl;
+  cout<<"Your average mark is : "<<average<<endl;
   
   return 0;
 }
