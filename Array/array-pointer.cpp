c5ffe7f17a9b938bd8a27459b940fb52769eccf6
Mahdi Hassan <mahdioffi333@gmail.com>
2025-09-20 17:51:34 +0600

    practice array

---
+++ Array/array-pointer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+//   int x = 10;
+//   int *ptr = &x;
+   
+//   int y = 25;
+//   ptr = &y;
+//   cout << *ptr << "\n"; // 25
+
+int arr[5];
+cout << arr << "\n";
+   
+    return 0;
+}
---
+++ Array/pointer-arithmetic-1.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+    // int a = 10;
+    // int *aptr = &a;
+    
+    // cout << aptr << "\n";
+    // aptr++;     // 1 int ++
+    // cout << aptr << "\n";
+    
+    
+    // ch
+    // char ch = 'a';
+    // int *ptr = &ch;
+    
+    // cout << ptr << "\n";
+    // ptr++;          // 1 ch ++
+    // cout << ptr << "\n";
+    
+    
+    int a = 5;
+    int *ptr = &a;
+    
+    cout << ptr = "\n";     // 1st 
+    ptr++;
+    cout << ptr = "\n";
+    ptr--;
+    cout << ptr = "\n";     // 3rd 
+    
+    return 0;
+}
