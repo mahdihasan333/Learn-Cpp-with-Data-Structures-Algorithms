798fd343ada48c312635ecfdbda24d5f030f251a
Mahdi Hassan <mahdioffi333@gmail.com>
2025-09-22 17:37:07 +0600

    practice array

---
+++ Array/max-sum-subarray-slightly-optimized.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <climits>
+
+using namespace std;
+
+void maxSubarraySum(int *arr, int n){
+    int maxSum = INT_MIN;
+    
+    for(int start=0; start < n; start++){   // start = 2
+        int currSum = 0;
+        
+        for(int end=start; end < n; end++){ // end = 2, 3, 4, 5
+            currSum += arr[end];
+            maxSum = max(maxSum, currSum);
+        }
+    }
+    
+    cout << "maximum subarray sum = " << maxSum << endl;
+}
+
+int main() {
+    int arr[6] = {2, -3, 6, -5, 4, 2};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    maxSubarraySum(arr, n);
+    return 0;
+}
